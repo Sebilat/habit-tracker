d3c747a205a2b44929c7f56dce1b65ec5808efa2
Sebilat Gambo <sebigambo@gmail.com>
2026-01-09 12:23:54 -0500

    Day 1: Setup Django project and implement login/register/logout

--- tracker/views.py
+++ tracker/views.py
@@ -1,6 +1,64 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
 
-# Create your views here.
-def index (request) :
-    return render(request, 'tracker/index.html')
+# Home page view 
+def index(request):
+    return render(request, "tracker/index.html")
+
+# Register page view 
+def register(request):
+    if request.user.is_authenticated:
+        return redirect("index")
+    
+    if request.method == "POST":
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        # If passwords do not match  
+        if password1 != password2:
+            return render(request, "tracker/register.html", {
+                "message": "Passwords do not match."
+            })
+        
+        # Check if username exists
+        if User.objects.filter(username=username).exists():
+            return render(request, "tracker/register.html", {
+                "message": "Username already exists."
+            })
+        
+        # Create user
+        user = User.objects.create_user(username=username, password=password1)
+        user.save() 
+        login(request, user)
+        return redirect("index")
+            
+    return render(request, "tracker/register.html")
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect("index")
+    
+    message = None
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("index")
+        else:
+            message = "Invalid username or password."
+
+    return render(request, "tracker/login.html", {"message": message})
+
+
+def logout_view(request):
+
+    # Logout the currently logged-in user and clear out their session
+    logout(request)
+
+    # After logging out, send user back to the home page
+    return redirect("index")
